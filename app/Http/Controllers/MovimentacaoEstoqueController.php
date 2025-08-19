bc343721f1adfa9d81b1cd001d5fde397f2ac778
gu0303 <ga.araujo0303@gmail.com>
2025-08-19 10:30:11 -0300

    Fechamento de chaves

--- app/Http/Controllers/MovimentacaoEstoqueController.php
+++ app/Http/Controllers/MovimentacaoEstoqueController.php
@@ -81,11 +81,13 @@ class MovimentacaoEstoqueController extends Controller
                 $item->increment('quantidade', $request->quantidade);
             } else {
                 $item->decrement('quantidade', $request->quantidade);
-            });
+            }
 
         return redirect()->route('movimentacoes.index')
             ->with('success', 'Movimentação registrada com sucesso!');
-
+        });
+    }
+    
     /**
      * Display the specified resource.
      */
